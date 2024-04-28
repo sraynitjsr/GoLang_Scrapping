ec9b9deb8385785445dfc7664adc358fdb3ed99a
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-28 11:57:40 +0530

    Parsing YAML Data

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type Person struct {
-	Name string `json:"name"`
-	Age  int    `json:"age"`
-	City string `json:"city"`
+	Name string `json:"name" yaml:"name"`
+	Age  int    `json:"age" yaml:"age"`
+	City string `json:"city" yaml:"city"`
 }
 
 func main() {
@@ -17,6 +19,9 @@ func main() {
 
 	fmt.Println("Parsing JSON Data")
 	parseJSONData()
+
+	fmt.Println("\nParsing YAML Data")
+	parseYAMLData()
 }
 
 func parseJSONData() {
@@ -30,6 +35,28 @@ func parseJSONData() {
 	err = json.Unmarshal(data, &people)
 	if err != nil {
 		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	for _, p := range people {
+		fmt.Println("Name:", p.Name)
+		fmt.Println("Age:", p.Age)
+		fmt.Println("City:", p.City)
+		fmt.Println()
+	}
+}
+
+func parseYAMLData() {
+	data, err := os.ReadFile("DATA/yaml_data.yaml")
+	if err != nil {
+		fmt.Println("Error reading YAML file:", err)
+		return
+	}
+
+	var people []Person
+	err = yaml.Unmarshal(data, &people)
+	if err != nil {
+		fmt.Println("Error parsing YAML:", err)
 		return
 	}
 
