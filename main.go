34c05f98dfaf91531c6adf13498484427f28d7b0
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-04-28 11:47:35 +0530

    Parsing JSON Data

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Person struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	City string `json:"city"`
+}
 
 func main() {
-    fmt.Println("Scrapping Using GoLang")
+	fmt.Print("****Scrapping Using GoLang****\n\n")
+
+	fmt.Println("Parsing JSON Data")
+	parseJSONData()
+}
+
+func parseJSONData() {
+	data, err := os.ReadFile("DATA/json_data.json")
+	if err != nil {
+		fmt.Println("Error reading JSON file:", err)
+		return
+	}
+
+	var people []Person
+	err = json.Unmarshal(data, &people)
+	if err != nil {
+		fmt.Println("Error parsing JSON:", err)
+		return
+	}
+
+	for _, p := range people {
+		fmt.Println("Name:", p.Name)
+		fmt.Println("Age:", p.Age)
+		fmt.Println("City:", p.City)
+		fmt.Println()
+	}
 }
