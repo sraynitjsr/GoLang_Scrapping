7fe9ac0899a3e896707aaea4e7fd25ddea3f4336
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-05-04 11:27:42 +0530

    Scrapping Online RAW JSON Data

--- scrap_online_data.go
+++ scrap_online_data.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	url := "https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/Online_Data_Scrapping/data1.json"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Error making HTTP request:", err)
+	}
+	defer resp.Body.Close()
+
+	var result []interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		log.Fatal("Error parsing JSON:", err)
+	}
+
+	for _, obj := range result {
+		fmt.Println(obj)
+	}
+}
