a0ec1bf914992bdc09e6948823a610dd4382adf1
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-05-08 19:48:00 +0530

    Scrap Online JSON, CSV and YAML Data

--- scrap_online_data.go
+++ scrap_online_data.go
@@ -4,13 +4,17 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
 	scrapedJSONData := scrapeJSONData("https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/Online_Data_Scrapping/data1.json")
 	scrapedCSVData := scrapeCSVData("https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/CSV/csv_data.csv")
+	scrapedYAMLData := scrapeYAMLData("https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/YAML/yaml_data.yaml")
 
 	fmt.Println("Scraped JSON Data:")
 	for _, obj := range scrapedJSONData {
@@ -20,6 +24,11 @@ func main() {
 	fmt.Println("\nScraped CSV Data:")
 	for _, row := range scrapedCSVData {
 		fmt.Println(row)
+	}
+
+	fmt.Println("\nScraped YAML Data:")
+	for _, data := range scrapedYAMLData {
+		fmt.Println(data)
 	}
 }
 
@@ -53,4 +62,25 @@ func scrapeCSVData(url string) [][]string {
 	}
 
 	return records
+}
+
+func scrapeYAMLData(url string) []map[interface{}]interface{} {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Error making HTTP request:", err)
+	}
+	defer resp.Body.Close()
+
+	yamlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading YAML:", err)
+	}
+
+	var result []map[interface{}]interface{}
+	err = yaml.Unmarshal(yamlData, &result)
+	if err != nil {
+		log.Fatal("Error parsing YAML:", err)
+	}
+
+	return result
 }
