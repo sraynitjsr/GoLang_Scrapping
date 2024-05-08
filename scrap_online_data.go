b89b0549674bd731c28b08325ebd08dcc01d3db8
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-05-08 19:43:48 +0530

    Scrap Online JSON and CSV Data

--- scrap_online_data.go
+++ scrap_online_data.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -8,7 +9,21 @@ import (
 )
 
 func main() {
-	url := "https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/Online_Data_Scrapping/data1.json"
+	scrapedJSONData := scrapeJSONData("https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/Online_Data_Scrapping/data1.json")
+	scrapedCSVData := scrapeCSVData("https://raw.githubusercontent.com/sraynitjsr/GoLang_Scrapping/main/CSV/csv_data.csv")
+
+	fmt.Println("Scraped JSON Data:")
+	for _, obj := range scrapedJSONData {
+		fmt.Println(obj)
+	}
+
+	fmt.Println("\nScraped CSV Data:")
+	for _, row := range scrapedCSVData {
+		fmt.Println(row)
+	}
+}
+
+func scrapeJSONData(url string) []interface{} {
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatal("Error making HTTP request:", err)
@@ -21,7 +36,21 @@ func main() {
 		log.Fatal("Error parsing JSON:", err)
 	}
 
-	for _, obj := range result {
-		fmt.Println(obj)
+	return result
+}
+
+func scrapeCSVData(url string) [][]string {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal("Error making HTTP request:", err)
 	}
+	defer resp.Body.Close()
+
+	reader := csv.NewReader(resp.Body)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal("Error reading CSV:", err)
+	}
+
+	return records
 }
